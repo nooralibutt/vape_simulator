2db05e2c73c50ec1ac805c52b8bf9bf1afadcc22
Hanzla <mhanzla80@gmail.com>
2023-03-08 16:34:56 +0500

    add routes.

--- lib/screens/selection_screens/choose_flavour_screen.dart
+++ lib/screens/selection_screens/choose_flavour_screen.dart
@@ -5,6 +5,7 @@ import 'package:vape_simulator/widgets/base_scaffold.dart';
 import 'package:vape_simulator/widgets/my_elevated_button.dart';
 
 class ChooseFlavourScreen extends StatelessWidget {
+  static const String routeName = "/ChooseFlavourScreen";
   const ChooseFlavourScreen({Key? key}) : super(key: key);
 
   @override
